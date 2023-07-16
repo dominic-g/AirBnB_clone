384de9adfb9d8e563ce46b3ec7bc87e97021be4a
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-07-16 10:19:35 -0400

    BASE & FILE_STORAGE

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# File: __init__.py
+# Main Authors: Dominic Gitau - Priscah Monicah
+# email(s): <dominicnjoroge1@gmail.com>
+#           <prisca2920@gmail.com>
+#           
+
+"""This module is A unique FileStorage/DBStorage instance for all models"""
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
+storage = DBStorage() if os.getenv(
+    'AIRBNB_TYPE_STORAGE') == 'db' else FileStorage()
+
+storage.reload()
